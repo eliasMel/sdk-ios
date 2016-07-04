0073b5636bde890f3fa832ac54a45c4807278711
Maven Build User <maven_user@lixar.com>
2016-07-04 18:24:52 -0400

    Automated Commit for changes

--- ProxSeeSDK/LXProxSeeSDK.framework/Headers/LXProxSeeSDK.h
+++ ProxSeeSDK/LXProxSeeSDK.framework/Headers/LXProxSeeSDK.h
@@ -15,10 +15,10 @@ FOUNDATION_EXPORT NSString * const LXProxSeeSDKBuildString;
 
 
 //  Version: 2.3.2
-//  Build String : -B29_master.88cdf17e875
+//  Build String : -B30_master.88cdf17e875
 //
 //  Built by: bamboo
-//        at: Mon  4 Jul 2016 17:41:37 EDT
+//        at: Mon  4 Jul 2016 18:15:37 EDT
 
 // In this header, you should import all the public headers of your framework using statements like #import <LXProximitySDK/PublicHeader.h>
 
